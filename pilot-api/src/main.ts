9f1354242cda151f4424995b63280c7820f44ce8
Alex Cioarec <cioarec.alexandru96@gmail.com>
2025-04-29 21:05:30 +0300

    Cors allow everything

--- pilot-api/src/main.ts
+++ pilot-api/src/main.ts
@@ -6,7 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const config = configService.get<ConfigDto>('config');
