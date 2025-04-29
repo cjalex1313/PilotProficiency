4a321ace87f4a231f7659eeb2a489a00588721ba
Alex Cioarec <cioarec.alexandru96@gmail.com>
2025-04-29 21:25:48 +0300

    Try fix cors

--- pilot-api/src/main.ts
+++ pilot-api/src/main.ts
@@ -7,7 +7,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
